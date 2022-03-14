fee7b1fd368fe134f3c28a3319f02fefbbccc98d
prakash <prakashtech250@gmail.com>
2022-03-14 22:15:15 +0545

    v1.1

--- spider.py
+++ spider.py
@@ -1,4 +1,3 @@
-from gc import get_referents
 import requests
 import json
 import time
@@ -28,7 +27,7 @@ class myntraApi():
             self.thread_limit = 1
         if self.items_to_scrape == '':
             self.items_to_scrape = 'all'
-        title = ['Crawling Time','Product Rank','Product Url','Category','Name','Brand','Product Id','Description','Seller','Average Rating','Total Rating','Total Reviews','Star1 Count','Star2 Count','Star3 Count','Star4 Count','Star5 Count','List Price','Sale Price','Product Details','Fit','Product Image']
+        title = ['Crawling Time','Product Rank','Product Url','Category','Name','Brand','Product Id','Description','Seller','Average Rating','Total Rating','Total Reviews','Star1 Count','Star2 Count','Star3 Count','Star4 Count','Star5 Count','List Price','Sale Price','Product Details','Fit','Material','Product Image']
         self.page_no = 1
         self.product_count = 0
         if os.path.exists(self.filename):
@@ -78,7 +77,8 @@ class myntraApi():
             return cookies
 
     def process_item(self, item):
-        line = [item['Crawling Time'], item['Product Rank'] ,item['Product Url'], item['Category'], item['Name'], item['Brand'], item['Product Id'], item['Description'], item['Seller'], item['Average Rating'], item['Total Rating'], item['Total Reviews'],item['Star1 Count'], item['Star2 Count'], item['Star3 Count'], item['Star4 Count'], item['Star5 Count'], item['List Price'], item['Sale Price'], item['Product Details'], item['Fit'], item['Product Images']]
+        # self.product_count +=1
+        line = [item['Crawling Time'], item['Product Rank'] ,item['Product Url'], item['Category'], item['Name'], item['Brand'], item['Product Id'], item['Description'], item['Seller'], item['Average Rating'], item['Total Rating'], item['Total Reviews'],item['Star1 Count'], item['Star2 Count'], item['Star3 Count'], item['Star4 Count'], item['Star5 Count'], item['List Price'], item['Sale Price'], item['Product Details'], item['Fit'],item['Material'] ,item['Product Images']]
         self.ws.append(line)
         self.wb.save(self.filename)
 
@@ -96,7 +96,7 @@ class myntraApi():
                 if response.status_code == 200:
                     break
             except:
-                print('Error in connection....Trying again')
+                print('    Error in connection....Trying again')
                 time.sleep(0.5)
         return response
 
@@ -112,6 +112,7 @@ class myntraApi():
         }
         reviews_count = 0
         seller_name = ''
+        material = ''
         response = self.get_requests(url)
         sel = Selector(text=response.text)
         scriptCode = sel.css('script::text').extract()
@@ -125,8 +126,13 @@ class myntraApi():
                     title = product_detail.get('title')
                     if 'product details' in title.lower():
                         description = product_detail.get('description')
+                        description = self.cleanhtml(description)
                     if 'fit' in title.lower():
                         fit = product_detail.get('description')
+                        fit = self.cleanhtml(fit)
+                    if 'material' in title.lower():
+                        material = product_detail.get('description')
+                        material = self.cleanhtml(material)
                 try:
                     rating_Infos = product_details['ratings']['ratingInfo']
                     for rating_Info in rating_Infos:
@@ -140,17 +146,23 @@ class myntraApi():
                     reviews_count = product_details['ratings']['reviewInfo']['reviewsCount']
                 except:
                     pass
-                seller_name = product_details['sellers'][0]['sellerName']
-        return description, fit, stars, reviews_count, seller_name
+                seller_name = product_details['sellers'][0]['sellerName']  
+        return description, fit, stars, reviews_count, seller_name, material
 
+    def cleanhtml(self, raw_html):
+        CLEANR = re.compile('<.*?>') 
+        cleantext = re.sub(CLEANR, ' ', raw_html)
+        return cleantext
+ 
     def get_product(self, product):
-        self.product_count += 1
         name = product.get('productName')
+        self.product_count += 1
+        product['rank'] = self.product_count
         productUrl = 'https://www.myntra.com/' + product.get('landingPageUrl')
         print(f'{self.product_count}: {name}')
         if productUrl in self.done:
             return
-        product_details, fit, stars, totalReviews, seller = self.product_details(productUrl)
+        product_details, fit, stars, totalReviews, seller, material = self.product_details(productUrl)
         category = product.get('category')
         brand = product.get('brand')
         productId = product.get('productId')
@@ -169,7 +181,7 @@ class myntraApi():
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         scraped_info = {
             'Crawling Time': dt_string,
-            'Product Rank': self.product_count,
+            'Product Rank': product.get('rank'),
             'Product Url': productUrl,
             'Category': category,
             'Name': name,
@@ -189,6 +201,7 @@ class myntraApi():
             'Sale Price': sale_price,
             'Product Details': product_details,
             'Fit': fit,
+            'Material': material,
             'Product Images': ','.join(product_images)
         }
         self.process_item(scraped_info)
